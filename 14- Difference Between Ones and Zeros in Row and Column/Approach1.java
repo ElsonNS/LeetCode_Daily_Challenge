95cc6eb0f3177c6e68a50cce75d78a00f2c53d68
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-12-14 19:20:08 +0530

    Add files via upload

---
+++ 14- Difference Between Ones and Zeros in Row and Column/Approach1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int[] rowOnes = new int[grid.length];
+        int[] colOnes = new int[grid[0].length];
+        int [][] diff = new int [grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++)
+        {
+            for (int j = 0; j < grid[0].length; j++)
+            {
+                rowOnes[i] += grid[i][j];
+                colOnes[j] += grid[i][j];
+            }
+        }
+        for (int i = 0; i < grid.length; i++)
+        {
+            for (int j = 0; j < grid[0].length; j++)
+            {
+                diff[i][j] = 2 * (rowOnes[i] + colOnes[j]) - grid.length - grid[0].length;
+            }
+        }
+        return diff;
+    }
+}
