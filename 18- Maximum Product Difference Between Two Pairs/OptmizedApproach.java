c26de7ebb723d3b3e37cab6ba342de187a331b5f
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2023-12-18 23:00:58 +0530

    Add files via upload

---
+++ 18- Maximum Product Difference Between Two Pairs/OptmizedApproach.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class OptmizedApproach {
+    public int maxProductDifference(int[] nums) {
+        int[] cloneNums = Arrays.copyOf(nums, nums.length);
+        int[] fmaxmin = getMaxMin(cloneNums);
+        int[] smaxmin = getMaxMin(cloneNums);
+
+        return fmaxmin[0] * smaxmin[0] - fmaxmin[1] * smaxmin[1];
+    }
+
+    private int[] getMaxMin(int[] nums) {
+        int len = nums.length;
+        int maxi = 0, mini = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+                maxi = i;
+            }
+            if (nums[i] < min && nums[i] != Integer.MIN_VALUE) {
+                min = nums[i];
+                mini = i;
+            }
+        }
+        nums[maxi] = Integer.MIN_VALUE;
+        nums[mini] = Integer.MIN_VALUE;
+        return new int[] {max, min};
+    }
+}
+    
