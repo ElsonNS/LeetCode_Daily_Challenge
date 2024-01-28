c61c34691313d1cd14b0665d4606e5490284f7cc
Elson Nag <99711378+ElsonNS@users.noreply.github.com>
2024-01-28 23:21:40 +0530

    Update hashMapApproach.java

--- January_2024/8. Contains Duplicates/hashMapApproach.java
+++ January_2024/8. Contains Duplicates/hashMapApproach.java
@@ -6,10 +6,12 @@ class Solution {
         Map<Integer, integer> seen = new HashMap<>();
         for (int num :  nums)
         {
+            //Checking if the element of array is already present in hashmap and has frequency greater than 1 or equal to 1
             if(seen.containsKey(num) && seen.get(num) >= 1)
             {
                 return true;
             }
+            //Storing values of array in hashmap
             seen.put(num, seen.getOrDefault(num, 0)+1);
         }
         return false;
